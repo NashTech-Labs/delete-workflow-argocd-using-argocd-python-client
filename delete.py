740257e641bdb423480fb04029f9fde160f123cf
knoldus <naincy.kumari@knoldus.com>
2022-08-25 13:37:27 +0530

    added files

---
+++ delete.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+import argo_workflows
+from argo_workflows.api import workflow_service_api
+
+configuration = argo_workflows.Configuration(host="https://127.0.0.1:2746")
+configuration.verify_ssl = False
+
+
+api_client = argo_workflows.ApiClient(configuration)
+
+with argo_workflows.ApiClient(configuration) as api_client:
+    api_instance = workflow_service_api.WorkflowServiceApi(api_client)
+    namespace = "argo" 
+    name= "awesome-dragon"
+    try:
+        api_response = api_instance.delete_workflow(namespace, name)
+        pprint(api_response)
+    except argo_workflows.ApiException as e:
+        print("Exception when calling WorkflowServiceApi->delete_workflow: %s\n" % e)
